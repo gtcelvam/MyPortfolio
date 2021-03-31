103f216b1e83473d812d0ecc8a6f2c5194906fb3
Gtcelvam <selvasizzler@gmail.com>
2021-03-31 12:28:14 +0530

    hover effects

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ class Main extends Component{
                         <p>Here's what I'm <span>|</span> What I do</p><br/>
                         <div className="btn-div">
                             <button className="btn"><a href="https://drive.google.com/file/d/1vKebQiUfDEElA45_knSq_FwYcPKz4QEq/view?usp=sharing">Resume</a></button>
-                            <button className="btn"><a href="https://github.com/gtcelvam">Projects</a></button>
+                            <button className="btn"><a href="/project">Projects</a></button>
                         </div>
                         <p>Enthusiastic and self-motivated fresher Web designer. Eager to pursue career web development, UX, and visual design skills.</p>
                         <div className="info">
