c02578e4649c4ae63de40c112f4d0a65bbbf1276
Gtcelvam <selvasizzler@gmail.com>
2021-03-31 12:35:14 +0530

    Resume updated

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ class Main extends Component{
                         <h1>Hello</h1><br/>
                         <p>Here's what I'm <span>|</span> What I do</p><br/>
                         <div className="btn-div">
-                            <button className="btn"><a href="https://drive.google.com/file/d/1vKebQiUfDEElA45_knSq_FwYcPKz4QEq/view?usp=sharing">Resume</a></button>
+                            <button className="btn"><a href="https://drive.google.com/file/d/1RARzvVI59IklAjejHX2TxvFD_B486zWF/view?usp=sharing">Resume</a></button>
                             <button className="btn"><a href="/project">Projects</a></button>
                         </div>
                         <p>Enthusiastic and self-motivated fresher Web designer. Eager to pursue career web development, UX, and visual design skills.</p>
