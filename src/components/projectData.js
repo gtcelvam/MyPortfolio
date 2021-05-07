46b85b51d9fa8724fe7202533ed671d787d1d185
Gtcelvam <selvasizzler@gmail.com>
2021-05-07 12:36:56 +0530

    Billing Project added

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -8,34 +8,41 @@ var Projects = [
     },
     {
         id : 2,
+        img : "./images/Billing.PNG",
+        name : "Billing and Invoice",
+        des : "Using React js and React hooks",
+        link : "https://billing-and-invoice.web.app/"
+    },
+    {
+        id : 3,
         img : "./images/Restautant_Site.PNG",
         name : "Restaurant Website",
         des : "Using HTML, CSS and vanilla JS",
         link : "https://restaurantwebsite-ts.web.app"
     },
     {
-        id : 3,
+        id : 4,
         img : "./images/Photo_Search.PNG",
         name : "Photo Search",
         des : "Rest API from Unsplash, Node js and EJS",
         link : "https://github.com/gtcelvam/Simple-Website_Node.js"
     },
     {
-        id : 4,
+        id : 5,
         img : "./images/Weather_App.PNG",
         name : "Weather App",
         des : "Using Rest API Node js and EJS",
         link : "https://github.com/gtcelvam/Weather_App"
     },
     {
-        id : 5,
+        id : 6,
         img : "./images/College_Site.PNG",
         name : "College Website",
         des : "Using HTML, CSS and vanilla JS",
         link : "https://collegewebsite-ts.web.app"
     },
     {
-        id : 6,
+        id : 7,
         img : "./images/Hotel_Site.PNG",
         name : "Hotel Website",
         des : "Using HTML, CSS and vanilla JS",
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ class Main extends Component{
                 <div className="c-right">
                     <div className="profile">
                         <div className="img-box">
-                            <img src="https://media-exp1.licdn.com/dms/image/C5103AQGNyqTKOPMfrQ/profile-displayphoto-shrink_800_800/0/1551808503092?e=1621468800&v=beta&t=sEC8SqFStu1dx5OP44JAHIcxGSzzXlt7A_qPY_kHHKM" id="img" alt="myimg"/>
+                            <img src="./images/gts.JPG" id="img" alt="myimg"/>
                         </div>
                         <div className="details">
                             <h1>Thamarai selvan</h1>
@@ -20,7 +20,7 @@ class Main extends Component{
                         </div>
                         <div className="icons">
                             <ul>
-                                <li><a href="https://www.facebook.com/thamarai.selvan.3975"><i className="fab fa-facebook"></i></a></li>
+                                <li><a href="http://api.whatsapp.com/send?phone=+919698229654"><i class="fab fa-whatsapp"></i></a></li>
                                 <li><a href="https://www.linkedin.com/in/thamarai-selvan-851068164/"><i className="fab fa-linkedin"></i></a></li>
                                 <li><a href="https://github.com/gtcelvam"><i className="fab fa-github"></i></a></li>
                             </ul>
@@ -34,7 +34,7 @@ class Main extends Component{
                             <button className="btn"><a href="https://drive.google.com/file/d/1668RWTLMKr6RP3-kDzMgPFdjD8_F6ESI/view?usp=sharing">Resume</a></button>
                             <button className="btn" id="btn-projects"><a href="/project">Projects</a></button>
                         </div>
-                        <p>Enthusiastic and self-motivated fresher Web designer. Eager to pursue career web development, UX, and visual design skills.</p>
+                        <p>Enthusiastic and self-motivated fresher Web Developer.Eager to pursue career on web development with some relevent skills on Full stack development.</p>
                         <div className="info">
                             <p className="info-details"><i className="fa fa-map-marker" aria-hidden="true"></i>Chennai</p>
                             <p className="info-details"><i className="fa fa-mobile" aria-hidden="true"></i>+919489029654</p>
